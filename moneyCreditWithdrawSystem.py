d8057af48835c88d546f1d0b20dff92605211f96
Supriya-Putta <158039624+Supriya-Putta@users.noreply.github.com>
2024-07-25 18:00:57 +0530

    Money_Credit_Withdraw_System

---
+++ moneyCreditWithdrawSystem.py
@@ -0,0 +1,11 @@
+class moneyCreditWithdrawSystem :
+    pass
+    if __name__ == '__main__':
+        print('Hi, Welocme to Money Credit Withdrawl System')
+        print('I am assesting for credit and withdraw money from your bank account')
+        print('''Please select the your options from below
+              1. Enter 1 for Registration 
+              2. Enter 2 for Login
+              ''')
+        registraion_login_cmd = int(input())
+        print(f"{registraion_login_cmd}")
