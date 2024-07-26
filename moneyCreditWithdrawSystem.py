bf2d14c1a83f9aec07a5c5834999ae7355a7545b
Supriya-Putta <158039624+Supriya-Putta@users.noreply.github.com>
2024-07-26 22:46:03 +0530

    Exception handling

--- moneyCreditWithdrawSystem.py
+++ moneyCreditWithdrawSystem.py
@@ -8,19 +8,31 @@ class moneyCreditWithdrawlSystem:
             print('''Please select your options in below
                 1.Enter 1 for Registration
                 2.Enter 2 for Login
-                ''')
-            self.enter_process = int(input()) 
-            print(self.enter_process)
+                ''') 
+            try:
+               self.enter_process = int(input())
+            except Exception as e:
+                self.entrance_retry += 1
+                if self.entrance_retry != 3:
+                    print(f"Please enter only numeric values. Your input {str(e).split()[-1]} is not valid")
+                 
+                self.entrance_process_init()                  
+                return False
+           # print(self.enter_process)
             if self.enter_process == 1:
                 print("Navigating to Registration")
             elif self.enter_process == 2:
                 print("Navigating to Login")
             else :
-                print("Please enter correct entrance process number as 1 or 2")
+                self.entrance_retry += 1
+                if self.entrance_retry != 3:
+                    print("Please enter correct entrance process number as 1 or 2")
+                
                 self.entrance_process_init()
+                
         else : 
             print("Entrance process retries exceeded over 3 times. Unfortunately we can't process your request due to incorrect input")        
-
+            
 # Defining a main Function.
 # The project process starts from here
 if __name__ == '__main__':
